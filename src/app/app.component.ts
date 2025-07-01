0f63431276977b7fab32fa1c7d4a9c6df8d7270f
Stefano Amenta <git.stefano@outlook.it>
2025-07-01 21:44:14 +0200

    TypewriterService: implementato servizio per la scrittura automatica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-
+import { TypewriterService } from './shared/services/typeWrite/type-writer-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'customCv';
+  displayedText = '';
 
-  constructor(private translate: TranslateService) {
+  constructor(
+    private translate: TranslateService,
+    private typewriterService: TypewriterService
+  ) {
     translate.setDefaultLang('it');
+  }
+  ngOnInit() {
+    const fullText = 'Ciao! Sto scrivendo in automatico... 🚀';
+
+    this.typewriterService.typeText(
+      fullText,
+      (current) => {
+        this.displayedText = current;
+      },
+      80, // velocità in ms
+      () => {
+        console.log('Scrittura completata!');
+      }
+    );
   }
 }
