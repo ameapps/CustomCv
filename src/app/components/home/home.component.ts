cc489fa7654bdeb3a55ee945e807d3169769e5d8
stefanoyoyo <git.stefano@outlook.it>
2025-08-01 13:30:23 +0200

    Home: titolo dinamico

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeTexts } from 'src/app/shared/models/home/home-texts';
+import { TypewriterService } from 'src/app/shared/services/typeWrite/type-writer-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  texts: HomeTexts = {
+    name: '',
+    shortDescr: '',
+    longDescr: ''
   }
 
+  constructor(private typewriterService: TypewriterService) { }
+
+  ngOnInit(): void {
+    this.writeTextAutomatically('Boss', 'name');
+    // this.writeTextAutomatically('Code Wizard', 'shortDescr');
+    // this.writeTextAutomatically('Software Engenieer', 'shortDescr');
+    // this.writeTextAutomatically('Web Developer', 'shortDescr');
+  }
+
+  private writeTextAutomatically(finalText: string, refText: keyof HomeTexts) {
+    try {
+      const fullText = finalText;
+      this.typewriterService.typeText(
+        fullText,
+        (current) => {
+          this.texts[refText] = current;
+        },
+        80, // velocità in ms
+        () => {
+          console.log('Scrittura completata!');
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
