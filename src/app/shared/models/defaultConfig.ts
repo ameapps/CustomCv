df0339766753c6b4f94b2e73ad0c00a9549404af
ameapps <git.stefano@outlook.it>
2025-08-12 14:13:14 +0200

    init modello config

--- src/app/shared/models/defaultConfig.ts
+++ src/app/shared/models/defaultConfig.ts
@@ -1,4 +1,28 @@
 
 export class DefaultConfig {
+    common!: DefaultConfigCommon;
+    pages!: DefaultConfigPages;
+}
+
+export class DefaultConfigCommon {
+    header!: DefaultConfigCommonHeader;
+};
+export class DefaultConfigPages {
+    home!: DefaultConfigPagesHome;
+    about!: DefaultConfigPagesAbout;
+    contact!: DefaultConfigPagesContact;
+    experience!: DefaultConfigPagesExperience;
+    project!: DefaultConfigPagesProject;
+    skills!: DefaultConfigPagesSkills;
+};
+
+export class DefaultConfigPagesHome {}
+export class DefaultConfigPagesAbout {}
+export class DefaultConfigPagesContact {}
+export class DefaultConfigPagesExperience {}
+export class DefaultConfigPagesProject {}
+export class DefaultConfigPagesSkills {}
+
+export class DefaultConfigCommonHeader {
 
 }
