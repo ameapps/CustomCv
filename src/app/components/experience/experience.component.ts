1bc5fd84039bcee0d5fec26197d3919425bdee69
Stefano Amenta <git.stefano@outlook.it>
2025-09-04 15:07:49 +0200

    experience

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+  workExp: WorkExperience[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.workExp = [
+      {
+        title: 'AAA',
+        description: 'description',
+        isClicked: false,
+        dateFrom: 'mm/yy',
+        dateTo: 'mm/yy'
+      },
+      {
+        title: 'BBB',
+        description: 'description',
+        isClicked: false,
+        dateFrom: 'mm/yy',
+        dateTo: 'mm/yy'
+      },
+      {
+        title: 'CCC',
+        description: 'description',
+        isClicked: false,
+        dateFrom: 'mm/yy',
+        dateTo: 'mm/yy'
+      },
+      {
+        title: 'DDD',
+        description: 'description',
+        isClicked: false,
+        dateFrom: 'mm/yy',
+        dateTo: 'mm/yy'
+      },
+    ];
   }
 
+  onClickDot(experience: WorkExperience) {
+    this.workExp.forEach(exp => {
+      exp.isClicked = false;
+    });
+    experience.isClicked = true;
+  }
+
+}
+
+export class WorkExperience {
+  title = '';
+  description = '';
+  isClicked = false;
+  dateFrom = 'mm/yy';
+  dateTo = 'mm/yy';
 }
